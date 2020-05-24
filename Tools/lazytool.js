10d52f1d5bbdc79fec964b93df027998462a17e9
Nghia Tran <tnghia99@gmail.com>
2020-05-24 15:59:49 +0700

    Modify gitignore & update file name script

--- Tools/lazytool.js
+++ Tools/lazytool.js
@@ -1,8 +1,21 @@
 // browser bookmark to easily generate a file name without any spaces
 
-javascript:(function(){
-    let s = prompt("Enter name: ").replace(/ /g,'');
-    let h = prompt("Difficulty: ").toUpperCase();
-    prompt("Result: ", h + '_'+ s);
+javascript: (function () {
+  let s = document
+    .querySelector("div[data-cy='question-title']")
+	.textContent
+	.replace(/ /g, "");
+  let h = "";
+  switch (document.querySelector("div[diff]").textContent) {
+    case "Easy":
+      h = "E";
+      break;
+    case "Medium":
+      h = "M";
+      break;
+    case "Hard":
+      h = "H";
+      break;
+  }
+  prompt("Result: ", h.toUpperCase() + "_" + s);
 })();
-
